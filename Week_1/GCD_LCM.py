b5bccad273b419a0c88ca2fb4b885fc6c2818681
Samarth-Baronia <61177336+Samarth-Baronia@users.noreply.github.com>
2023-11-09 08:20:56 +0530

    Add files via upload

--- Week_1/GCD_LCM.py
+++ Week_1/GCD_LCM.py
@@ -1,14 +1,18 @@
 #URL = https://practice.geeksforgeeks.org/problems/lcm-and-gcd4516/1
 
-def lcmAndGcd(self, num1 , num2):
-    # code here 
-    a = num1
-    b = num2
-    while num1 != num2:
-        if num1 > num2:
-            num1 -= num2
-        else:
-            num2 -= num1
-       
-    lcm = (a*b) //  num1
-    return lcm, num1
+class Solution:
+    def lcmAndGcd(self, num1 , num2):
+        # code here
+        def find_gcd(x, y):
+            while y:
+                x, y = y, x % y
+            return x
+        
+        # Calculate GCD
+        gcd = find_gcd(num1, num2)
+        
+        # Calculate LCM using the formula: LCM(a, b) = (a * b) / GCD(a, b)
+        lcm = (num1 * num2) // gcd
+        
+        return lcm, gcd
+                
